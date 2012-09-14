84a8fe3a245ba47c20553fbae46e6da9ec80108d
andrew_jacobs <andrew_jacobs@9f70f953-3e06-4233-895a-d421fba01c0e>
2012-09-14 21:06:36 +0000

    Removed unnecessary import

--- trunk/src/uk/co/demon/obelisk/xasm/FileSource.java
+++ trunk/src/uk/co/demon/obelisk/xasm/FileSource.java
@@ -24,7 +24,6 @@ package uk.co.demon.obelisk.xasm;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
